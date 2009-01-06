9d8c39d4255b99a1a97d6121239f2db9c71c8687
Daniel Raffel <daniel.raffel@gmail.com>
2009-01-05 20:47:57 -0800

    minor polish, smaller fonts...

--- index.php
+++ index.php
@@ -5,9 +5,8 @@
 
 <img class="pic us" src="http://farm4.static.flickr.com/3175/3004087329_e39781584a.jpg">
 
-<h1>We met in NYC in Aug 2003 and fell in love.<p>Two years later we moved to San Francisco together.<p>In Nov 2008, while back in NYC for a short vacation, we became engaged.<p>Our wedding is on <a href="wedding">September 13, 2009</a>.</h1>
+<h1>We met in New York City in August 2003 and fell in love.  Two years later we moved to San Francisco together.<p>In November 2008, while back in New York City for a short vacation, we became engaged.  Our wedding is on <a href="wedding">September 13, 2009</a>.</h1>
 
 <?php
-	$page = 'registry';
 	include 'templates/footer.php';
 ?>
--- registry/index.php
+++ registry/index.php
@@ -5,9 +5,8 @@
 
 <h1>We are registered at:</h1>
 
-<h4><a href="http://www.heathceramics.com/heath/store/index.cfm?fuseaction=shopping.giftregistry&do=display&giftregistry_ID=3103" rel="external">Heath Ceramics</a></h4>
+<h3><a href="http://www.heathceramics.com/heath/store/index.cfm?fuseaction=shopping.giftregistry&do=display&giftregistry_ID=3103" rel="external">Heath Ceramics</a></h3>
 
 <?php
-	$page = 'registry';
 	include '../templates/footer.php';
 ?>
