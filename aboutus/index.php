6f75523411d86cdfaf02e9ba085e180c50f9d2e1
Daniel Raffel <daniel.raffel@gmail.com>
2009-01-05 18:02:36 -0800

    basically no changes testing

--- aboutus/index.php
+++ aboutus/index.php
@@ -3,7 +3,7 @@
 	include '../templates/header.php';
 ?>
 
-<h1>Daniel and Sara met in NYC in Aug 2003.  We fell in love and moved to San Francisco 2 years later.<p>In Nov 2008, while back in NYC for a short vacation, Daniel proposed.  Sara said Yes!<p>We are getting married in Chevy Chase, MD in September 2009.</h1>
+<h1>Daniel and Sara met in NYC in Aug 2003.  We fell in love and moved to San Francisco 2 years later.<p>In Nov 2008, while back in NYC for a short vacation, Daniel proposed.  Sara said YES!<p>We are getting married in Chevy Chase, MD in September 2009.</h1>
 
 <h1>Should you have any questions about the wedding <a href="mailto:&#x75;&#x73;&#x40;&#x64;&#x61;&#x6E;&#x69;&#x65;&#x6C;&#x2D;&#x73;&#x61;&#x72;&#x61;&#x2E;&#x63;&#x6F;&#x6D;">feel free to email us</a>.</h1>
 
--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 	include 'templates/header.php';
 ?>
 
+
 <img class="pic us" src="http://farm4.static.flickr.com/3175/3004087329_e39781584a.jpg">
 
 <?php
