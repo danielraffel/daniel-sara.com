4a46c620cc51207992a4a566e4df9acd8b30b128
Daniel Raffel <daniel.raffel@gmail.com>
2009-01-05 19:54:49 -0800

    deleted two un-necessary files

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 
 <img class="pic us" src="http://farm4.static.flickr.com/3175/3004087329_e39781584a.jpg">
 
-<h1>Daniel and Sara met in NYC in Aug 2003.<p>We fell in love and moved to San Francisco 2 years later.<p>In Nov 2008, while back in NYC for a short vacation, we became engaged.<p>Our wedding is <a href="wedding">September 13, 2009</a>.</h1>
+<h1>Daniel and Sara met in NYC in Aug 2003.<p>We fell in love and moved to San Francisco 2 years later.<p>In Nov 2008, while back in NYC for a short vacation, we became engaged.<p>Our wedding is on <a href="wedding">September 13, 2009</a>.</h1>
 
 <?php
 	$page = 'registry';
