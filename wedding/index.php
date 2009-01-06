38477dd6bad0cdb9b7bffc2168b7e185db897dac
Daniel Raffel <daniel.raffel@gmail.com>
2009-01-05 21:10:50 -0800

    min-width is nice ;)

--- wedding/index.php
+++ wedding/index.php
@@ -6,7 +6,7 @@
 <h1>We are getting married at <A href="http://www.audubonnaturalist.org/default.asp?page=514" rel="external">Woodend Nature Sanctuary</a> in Chevy Chase, MD on September 13th, 2009 at 5pm. <a target="_blank" href="http://www.google.com/calendar/event?action=TEMPLATE&amp;tmeid=ZzhiYnE1dTBmMXRsOXBjYjgwOXI2OTJnbXMgdTBoMGVrbGdyZnVnOTB0ampoNW1rOWNrb3NAZw&amp;tmsrc=dTBoMGVrbGdyZnVnOTB0ampoNW1rOWNrb3NAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ"><img class="gimage" border=0 src="http://www.google.com/calendar/images/ext/gc_button1_en.gif"></a></h1>
 
 <center>
-<iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"  src="http://maps.google.com/maps?f=q&amp;hl=en&amp;geocode=&amp;q=8940+Jones+mill+rd+chevy+chase&amp;sll=37.0625,-95.677068&amp;sspn=38.944835,77.695313&amp;ie=UTF8&amp;om=0&amp;t=p&amp;g=8940+Jones+mill+rd+chevy+chase&amp;ll=39.013248,-77.063084&amp;spn=0.039681,0.090981&amp;z=14&amp;iwloc=addr&amp;output=embed&amp;s=AARTsJpAsNNfrdCLkLQ8cmulsvNwhSWVPg"></iframe><br /><small><a href="http://maps.google.com/maps?f=q&amp;hl=en&amp;geocode=&amp;q=8940+Jones+mill+rd+chevy+chase&amp;sll=37.0625,-95.677068&amp;sspn=38.944835,77.695313&amp;ie=UTF8&amp;om=0&amp;t=p&amp;g=8940+Jones+mill+rd+chevy+chase&amp;ll=39.013248,-77.063084&amp;spn=0.039681,0.090981&amp;z=14&amp;iwloc=addr&amp;source=embed" rel="external" style="font-family: Helvetica, Georgia, "Times New Roman", Times, serif">View Larger Map</a></small></center>
+<iframe width="518" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"  src="http://maps.google.com/maps?f=q&amp;hl=en&amp;geocode=&amp;q=8940+Jones+mill+rd+chevy+chase&amp;sll=37.0625,-95.677068&amp;sspn=38.944835,77.695313&amp;ie=UTF8&amp;om=0&amp;t=p&amp;g=8940+Jones+mill+rd+chevy+chase&amp;ll=39.013248,-77.063084&amp;spn=0.039681,0.090981&amp;z=14&amp;iwloc=addr&amp;output=embed&amp;s=AARTsJpAsNNfrdCLkLQ8cmulsvNwhSWVPg"></iframe><br /><small><a href="http://maps.google.com/maps?f=q&amp;hl=en&amp;geocode=&amp;q=8940+Jones+mill+rd+chevy+chase&amp;sll=37.0625,-95.677068&amp;sspn=38.944835,77.695313&amp;ie=UTF8&amp;om=0&amp;t=p&amp;g=8940+Jones+mill+rd+chevy+chase&amp;ll=39.013248,-77.063084&amp;spn=0.039681,0.090981&amp;z=14&amp;iwloc=addr&amp;source=embed" rel="external" style="font-family: Helvetica, Georgia, "Times New Roman", Times, serif">View Larger Map</a></small></center>
 
 <p class="line">
 	
@@ -22,7 +22,7 @@
 
 <p class="line">
 
-<h5>Should you have any questions <a href="mailto:&#x75;&#x73;&#x40;&#x64;&#x61;&#x6E;&#x69;&#x65;&#x6C;&#x2D;&#x73;&#x61;&#x72;&#x61;&#x2E;&#x63;&#x6F;&#x6D;">feel free to email us</a>.</h5>
+<h5>If you have any questions <a href="mailto:&#x75;&#x73;&#x40;&#x64;&#x61;&#x6E;&#x69;&#x65;&#x6C;&#x2D;&#x73;&#x61;&#x72;&#x61;&#x2E;&#x63;&#x6F;&#x6D;">feel free to email us</a>.</h5>
 
 <?php
 	include '../templates/footer.php';
