12ea9c8bbc8fc680e7da42f7433538aaa1558b8b
Daniel Raffel <daniel.raffel@gmail.com>
2009-01-29 08:16:19 -0800

    updated hotel info

--- wedding/index.php
+++ wedding/index.php
@@ -9,16 +9,21 @@
 <iframe width="518" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"  src="http://maps.google.com/maps?f=q&amp;hl=en&amp;geocode=&amp;q=8940+Jones+mill+rd+chevy+chase&amp;sll=37.0625,-95.677068&amp;sspn=38.944835,77.695313&amp;ie=UTF8&amp;om=0&amp;t=p&amp;g=8940+Jones+mill+rd+chevy+chase&amp;ll=39.013248,-77.063084&amp;spn=0.039681,0.090981&amp;z=14&amp;iwloc=addr&amp;output=embed&amp;s=AARTsJpAsNNfrdCLkLQ8cmulsvNwhSWVPg"></iframe><br /><small><a href="http://maps.google.com/maps?f=q&amp;hl=en&amp;geocode=&amp;q=8940+Jones+mill+rd+chevy+chase&amp;sll=37.0625,-95.677068&amp;sspn=38.944835,77.695313&amp;ie=UTF8&amp;om=0&amp;t=p&amp;g=8940+Jones+mill+rd+chevy+chase&amp;ll=39.013248,-77.063084&amp;spn=0.039681,0.090981&amp;z=14&amp;iwloc=addr&amp;source=embed" rel="external" style="font-family: Helvetica, Georgia, "Times New Roman", Times, serif">View Larger Map</a></small></center>
 
 <p class="line">
-	
-<h1>The following hotels are convenient for out of town guests:</h1>
 
-<h3><li><a href="http://bethesda.hyatt.com" rel="external">Bethesda Hyatt</a></h3>
-<h3><li><a href="http://www.hilton.com/en/hi/groups/personalized/DCASSHF-PRW-20090912" rel="external">Silver Spring Hilton</a></h3>
+	<h1>If you're flying into town:</h1>
+	<h3><li>Ronald Reagan National Airport (DCA) is about 12 miles from Chevy Chase.</h3>
+	<h3><li>Dulles International Airport (IAD) and Baltimore-Washington Airport (BWI) are each about a 35 minute drive to Chevy Chase.</h3>	
 
 <p class="line">
-<h1>If you're flying into town:</h1>
-<h3><li>Ronald Reagan National Airport (DCA) is about 12 miles from Chevy Chase.</h3>
-<h3><li>Dulles International Airport (IAD) and Baltimore-Washington Airport (BWI) are each about a 35 minute drive to Chevy Chase.</h3>
+	
+<h1>We have reserved blocks of rooms at the following hotels:</h1>
+
+<h3><li><a href="http://bethesda.hyatt.com" rel="external">Bethesda Hyatt</a> $129/night (tell them you are part of the Paul/Raffel wedding to get the group rate.)</h3>
+<h3><li><a href="http://www.hilton.com/en/hi/groups/personalized/DCASSHF-PRW-20090912" rel="external">Silver Spring Hilton</a> $99/night</h3>
+	
+<h4>Be sure to book your room by mid-August in order to lock in the group rates. Both hotels are situated right next to <a href="http://www.wmata.com" rel="external">Metro Stations</a>, making it very easy to get downtown to museums, monuments, etc.<h4>
+
+<h2>If you choose another hotel, please let us know where you will be staying.</h2>
 
 <p class="line">
 
