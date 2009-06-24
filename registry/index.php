f296402f38d8369857b2ed77d2d96fe167883c9f
Daniel Raffel <daniel.raffel@gmail.com>
2009-06-23 17:53:04 -0700

    updated with williams-sonoma

--- registry/index.php
+++ registry/index.php
@@ -7,6 +7,7 @@
 
 <h3><a href="http://www.amazon.com/gp/registry/wedding/2KLPXE4TVMY2K" rel="external">Amazon</a></h3>
 <h3><a href="http://www.heathceramics.com/heath/store/index.cfm?fuseaction=shopping.giftregistry&do=display&giftregistry_ID=3103" rel="external">Heath Ceramics</a></h3>
+<h3><a href="http://www.williams-sonoma.com/registry/guestsignin.cfm?showimages=1&regid=2361077" rel="external">Williams-Sonoma</a></h3>
 
 <?php
 	include '../templates/footer.php';
