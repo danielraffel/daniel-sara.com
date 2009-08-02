b7d759d3e8c008d00f24a1d94a535c742bba7153
Daniel Raffel <danielraffel@DeluxeBookPro.hsd1.ca.comcast.net>
2009-08-01 22:11:50 -0700

    made heath first and updated css min width

--- registry/index.php
+++ registry/index.php
@@ -5,8 +5,8 @@
 
 <h1>We are registered at:</h1>
 
-<h3><a href="http://www.amazon.com/gp/registry/wedding/2KLPXE4TVMY2K" rel="external">Amazon</a></h3>
 <h3><a href="http://www.heathceramics.com/heath/store/index.cfm?fuseaction=shopping.giftregistry&do=display&giftregistry_ID=3103" rel="external">Heath Ceramics</a></h3>
+<h3><a href="http://www.amazon.com/gp/registry/wedding/2KLPXE4TVMY2K" rel="external">Amazon</a></h3>
 <h3><a href="http://www.williams-sonoma.com/registry/guestsignin.cfm?showimages=1&regid=2361077" rel="external">Williams-Sonoma</a></h3>
 
 <?php
